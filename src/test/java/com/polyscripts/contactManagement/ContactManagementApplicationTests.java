6023e6d054e6405ccfbd6c82552f3fdc447119a4
manifico <ysoui@hotmail.fr>
2022-03-11 01:04:06 +0100

    initial commit

---
+++ src/test/java/com/polyscripts/contactManagement/ContactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyscripts.contactManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
