8560b65fd322c484d36309c6c90ff7be75e3a92a
manifico <ysoui@hotmail.fr>
2022-03-18 15:16:30 +0100

    add enterprises listing

---
+++ src/main/java/com/polyscripts/contactManagement/Controllers/EnterpriseController.java
@@ -0,0 +1,32 @@
+package com.polyscripts.contactManagement.Controllers;
+
+import com.polyscripts.contactManagement.Services.EnterpriseService;
+import com.polyscripts.contactManagement.models.Enterprise;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/enterprises")
+public class EnterpriseController {
+
+    public final EnterpriseService enterpriseService;
+
+    public EnterpriseController(EnterpriseService enterpriseService) {
+        this.enterpriseService = enterpriseService;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<Enterprise>> getAllContacts(
+            @RequestParam int offset,
+            @RequestParam int pageSize
+    ) {
+        Page<Enterprise> enterprises = enterpriseService.getAllEnterprisesWithPagination(offset, pageSize);
+        return new ResponseEntity<>(enterprises, HttpStatus.OK);
+    }
+
+}
